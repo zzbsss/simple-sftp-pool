988d71066c3d19c26b3ea70b0203ff6dd036e719
cn32260 <zhibin.zou@powersi.com.cn>
2022-10-22 14:32:22 +0800

    初始化组件

---
+++ src/main/java/com/zzb/simple/sftp/config/SftpProperties.java
@@ -0,0 +1,111 @@
+package com.zzb.simple.sftp.config;
+
+/**
+ * @author zzb
+ * @version 1.0
+ * @description:
+ * @date 2022/10/22 11:04
+ */
+
+import com.jcraft.jsch.ChannelSftp;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "sftp")
+public class SftpProperties {
+
+    /**
+     * sftp 主机地址
+     */
+    private String host;
+    /**
+     * sftp 端口
+     */
+    private int port = 22;
+    /**
+     * sftp 用户名
+     */
+    private String username;
+    /**
+     * sftp 密码
+     */
+    private String password;
+
+    private Pool pool = new Pool();
+
+    public static class Pool extends GenericObjectPoolConfig<ChannelSftp> {
+
+        private int maxTotal = DEFAULT_MAX_TOTAL;
+        private int maxIdle = DEFAULT_MAX_IDLE;
+        private int minIdle = DEFAULT_MIN_IDLE;
+
+        public Pool() {
+            super();
+        }
+        @Override
+        public int getMaxTotal() {
+            return maxTotal;
+        }
+        @Override
+        public void setMaxTotal(int maxTotal) {
+            this.maxTotal = maxTotal;
+        }
+        @Override
+        public int getMaxIdle() {
+            return maxIdle;
+        }
+        @Override
+        public void setMaxIdle(int maxIdle) {
+            this.maxIdle = maxIdle;
+        }
+        @Override
+        public int getMinIdle() {
+            return minIdle;
+        }
+        @Override
+        public void setMinIdle(int minIdle) {
+            this.minIdle = minIdle;
+        }
+
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Pool getPool() {
+        return pool;
+    }
+
+    public void setPool(Pool pool) {
+        this.pool = pool;
+    }
+}
